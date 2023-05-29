34c2d82dd1f87ddb9d35419fcc3196df89089083
Aysun YILDIRIM <82328345+aysunyildirim@users.noreply.github.com>
2023-05-29 12:51:00 +0300

    Add project files.

---
+++ YarınınTarihi/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int yıl = DateTime.Today.Year;
+int ay = DateTime.Today.Month;
+int gün = DateTime.Today.Day+1; //yarın
+
+Console.WriteLine(gün+"."+ay+"."+yıl);
+Console.ReadLine();
